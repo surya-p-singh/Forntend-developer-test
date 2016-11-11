6519719d473077fa465515204d05ab8beaf64af9
Surya Singh <surya.singh@thetrainline.com>
2016-11-11 11:07:29 +0000

    Added usage of moment

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import moment from 'moment';
 
 export default class Service extends Component {
 
     render(){
         const { service } = this.props;
         const { realTimeUpdatesInfo, scheduledInfo, serviceOperator, destinationList, serviceIdentifier, callingPatternUrl } = service;
-
+        const { time } = moment (scheduledInfo.scheduledTime);
         return(
             <li>
                 <Link>
                     <div>
+                        <span>{moment(scheduledInfo.scheduledTime).format('HH:mm')} </span>
                         <span>{serviceOperator} </span>
-                        <span>{serviceIdentifier}</span>
+                        <span></span>
                     </div>
                 </Link>
             </li>
